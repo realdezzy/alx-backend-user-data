61e885c9ed5d4d6bf55bf1963453855eb31f100d
realdezzy <realdezzy1@gmail.com>
2023-08-08 09:13:22 +0100

    style fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,17 +20,20 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,16 +10,17 @@ class Auth:
 
     def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
         """Checks if a path requires authentication"""
-        if (path == None or exclude_paths == None
-            or len(exclude_paths) == 0):
+
+        if (path is None or exclude_paths is None or
+                len(exclude_paths) == 0):
             return True
+
         mod_path = path if match(r'/api/v1/[A-Za-z]*/$', path) \
-                    else path + '/'
+            else path + '/'
         if mod_path in exclude_paths:
             return False
         else:
             return True
-
 
     def authorization_header(self, request=None) -> str:
         """Flask request object"""
