67bb2869d4b4ce4a4964ff412da53560d30a7864
realdezzy <realdezzy1@gmail.com>
2023-08-11 14:45:24 +0100

    session auth route

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,7 +23,7 @@ if auth:
     if auth == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
-    elif auth == 'sesssion_auth':
+    elif auth == 'session_auth':
         from api.v1.auth.session_auth import SessionAuth
         auth = SessionAuth()
     else:
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 """ Session authenticated view"""
-from flask import jsonify, abort
+from flask import jsonify, abort, request
+from os import getenv
 from api.v1.views import app_views
+from models.user import User
 
-@app_views.route('/auth_session/login/', methods=['POST'], slashes=False)
-def login(request):
+
+@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+def login():
     """ Login route"""
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+
+    userList = User.search({'email': email})
+    user = None
+    if len(userList) == 0:
+        return jsonify({ "error": "no user found for this email" }), 404
+
+    for checkUser in userList:
+        if checkUser.is_valid_password(password):
+            user = checkUser
+            break
+
+    if user is None:
+        return jsonify({ "error": "wrong password" }), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    cookie_name = getenv('SESSION_NAME')
+
+    response = jsonify(user.to_json())
+    response.set_cookie(cookie_name, session_id)
+
+    return response
