06a68b80f321ad43e23b08ba44560b91c48eee7e
realdezzy <realdezzy1@gmail.com>
2023-08-17 00:20:55 +0100

    reset token typo

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,7 +89,7 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """ Creates and returns the reset password token. """
         try:
-            user = self._db.find_user_by_email(email=email)
+            user = self._db.find_user_by(email=email)
             u_id = str(_generate_uuid())
             self._db.update_user(user.id, reset_token=u_id)
             return u_id
