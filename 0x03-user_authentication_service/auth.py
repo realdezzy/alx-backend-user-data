00933d01b76ed26e7e4e8ff223625f2c0d179b6a
realdezzy <realdezzy1@gmail.com>
2023-08-16 13:38:22 +0100

    generate

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,6 +21,7 @@ def _generate_uuid() -> str:
     """ Generate a unique and return as string"""
     return str(uuid4())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
